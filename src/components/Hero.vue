1771798f84e62495fd470b25ddf45faceb7f3b19
Deejay Easter <deejayeaster@live.com>
2021-05-11 23:55:07 -0400

    adding margin padding fix to hero mobile

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="hero-wrapper bg-sectionGrey p-28 pb-36">
+    <div class="hero-wrapper bg-sectionGrey px-12 md:px-28 py-20">
         <div class="lg:w-3/6 md:5/6 m-auto">
-            <h2 class="lg:text-5xl text-3xl p-12 font-bold">It doesn’t look like you’re there yet.</h2>
+            <h2 class="lg:text-5xl text-3xl md:p-12 font-bold">It doesn’t look like you’re there yet.</h2>
             <p class="lg:text-4xl text-2xl">Airport Name and Hotel Name are nearby, where would you like your food delivered to?</p>
             <Button msg="Choose Location" class="bg-white mt-16" />
         </div>
