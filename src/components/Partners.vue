dc4165f0db87deab899464c1232b0c7015ea6aec
Deejay Easter <deejayeaster@live.com>
2021-05-14 02:37:37 -0400

    safari partners fix

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="partner-wrapper px-4 md:py-8 py-6 bg-sectionGrey">
         <h3 class="mb-4 text-xl lg:text-5xl lg:my-10 font-bold">Our Partners!</h3>
-        <div class="overlay overflow-scroll overflow-y-hidden max-h-28 md:overflow-hidden">
-            <div class="partner-img-wrapper flex mb-20 md:justify-center">
+        <div class="overlay overflow-scroll overflow-y-hidden md:overflow-hidden">
+            <div class="partner-img-wrapper flex lg:justify-center">
                 <img src="../assets/images/1200px-Allegiant_Air_Logo.svg.png" alt="Allegiant Logo" class="partner-img w-20 m-4">
-                <img src="../assets/images/1200px-Spirit_Airlines_logo.svg@2x.png" alt="Allegiant Logo" class="partner-img w-20 m-4">
-                <img src="../assets/images/Southwest_Airlines-Logo.wine@2x.png" alt="Allegiant Logo" class="partner-img w-20 m-4">
+                <img src="../assets/images/1200px-Spirit_Airlines_logo.svg@2x.png" alt="Sirit Logo" class="partner-img w-20 m-4">
+                <img src="../assets/images/Southwest_Airlines-Logo.wine@2x.png" alt="Southwest Logo" class="partner-img w-20 m-4">
                 <img src="../assets/images/Cape_Air-Logo.wine@2x.png" alt="Allegiant Logo" class="partner-img w-20 m-4">
                 <img src="../assets/images/boutique@2x.png" alt="Allegiant Logo" class="partner-img w-20 m-4">
                 <img src="../assets/images/1280px-Delta_logo.svg@2x.png" alt="Allegiant Logo" class="partner-img w-20 m-4">
