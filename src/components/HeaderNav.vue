48106752707d1ea67501234476ff8db4e86f0f0c
Deejay Easter <deejayeaster@live.com>
2021-05-20 22:39:44 -0400

    added button

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -4,14 +4,14 @@
             <img src="../assets/images/AccompanyLogo@2x.png" alt="" class="lg:w-48 w-28 py-2 ml-6 lg:ml-8">
             <div :class="isOpen ? 'block' : 'hidden'" class="wrapper top-12 right-4 absolute md:static md:flex">
                 <nav id="nav" class="float-right bg-gray-300 md:bg-transparent">
-                    <div class="flex flex-col  md:flex-row border border-gray-400 relative border-opacity-60 md:border-none">
+                    <div class="flex flex-col  md:flex-row border border-gray-400 relative items-center border-opacity-60 md:border-none">
                         <div class="triangle-outline md:hidden"></div>
                         <div class="triangle-up md:hidden"></div>
                         <router-link to="/" class="lg:mx-8 md:mx-4 border-b md:border-none text-grayText2 bg-white whitespace-nowrap py-1 md:py-0 text-15px lg:text-xl" >Home</router-link>
                         <router-link to="/WhyUs" class="lg:mx-8 md:mx-4 border-b md:border-none text-grayText2 bg-white whitespace-nowrap py-1 md:py-0 text-15px lg:text-xl">Why Accompany?</router-link>
                         <router-link to="/Careers" class="lg:mx-8 md:mx-4 border-b md:border-none text-grayText2 bg-white whitespace-nowrap py-1 md:py-0 text-15px lg:text-xl">Careers</router-link>
-                        <router-link to="/ContactUs" class="lg:mx-8 md:mx-4 border-b md:border-none text-grayText2 bg-white whitespace-nowrap text-15px py-1 md:py-0 lg:text-xl">Contact Us</router-link>
-                        <div class="lg:mx-8 md:mx-4 border-b md:border-none text-black bg-white whitespace-nowrap text-15px md:hidden lg:text-xl px-24 py-1 md:py-0 font-light">ORDER NOW</div>
+                        <router-link to="/ContactUs" class="lg:ml-8 lg:mr-4 md:mx-4 border-b md:border-none text-grayText2 bg-white whitespace-nowrap text-15px py-1 md:py-0 lg:text-xl">Contact Us</router-link>
+                        <div class="lg:mx-4 md:mx-4 border-b md:border-none text-black whitespace-nowrap text-15px bg-white lg:text-lg px-24 py-1 md:py-4 md:my-2 md:px-8 md:bg-footerBlue font-normal">ORDER NOW</div>
                     </div>
                 </nav>
             <div class="smartSearch"></div>
@@ -36,6 +36,11 @@ export default {
     methods: {
         toggle() {
             this.isOpen = !this.isOpen
+        }
+    },
+    watch: {
+        '$route'() {
+            this.isOpen = false
         }
     }
 }
