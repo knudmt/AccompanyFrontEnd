b69ccf390039d7eb476fec5558298804e2f636a4
knudmt <knudmt@outlook.com>
2021-08-22 18:25:13 -0400

    added eslit ignore for prod build

--- src/components/orderpage/ProductList.vue
+++ src/components/orderpage/ProductList.vue
@@ -1,3 +1,4 @@
+/* eslint-disable */
 <template>
     <div>
         <span v-if="!hideMenu">
--- src/js/appItems.js
+++ src/js/appItems.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default class AppItems
 {
     constructor(description, price, quantity){
